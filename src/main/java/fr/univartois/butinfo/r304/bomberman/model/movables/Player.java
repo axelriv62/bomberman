9b4385b6609020a595830c5326e392b966c536bc
Bourhim Bylel <bylel_bourhim@ens.univ-artois.fr>
2024-10-02 09:56:09 +0200

    #7

---
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/movables/Player.java
@@ -0,0 +1,89 @@
+package fr.univartois.butinfo.r304.bomberman.model.movables;
+
+import fr.univartois.butinfo.r304.bomberman.model.BombermanGame;
+import fr.univartois.butinfo.r304.bomberman.model.IMovable;
+import fr.univartois.butinfo.r304.bomberman.view.Sprite;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Player extends AbstractMovable{
+
+    /**
+     * Stocker le score.
+     */
+    private IntegerProperty score = new SimpleIntegerProperty(0);
+
+    /**
+     * Point de vie.
+     */
+    private IntegerProperty life = new SimpleIntegerProperty(3);
+
+    /**
+     * Crée une nouvelle instance de AbstractMovable.
+     *
+     * @param game      Le jeu dans lequel l'objet évolue.
+     * @param xPosition La position en x initiale de l'objet.
+     * @param yPosition La position en y initiale de l'objet.
+     * @param sprite    L'instance de {@link Sprite} représentant l'objet.
+     */
+    protected Player(BombermanGame game, double xPosition, double yPosition, Sprite sprite) {
+        super(game, xPosition, yPosition, sprite);
+    }
+
+
+    public int getScore() {
+        return score.get();
+    }
+
+    public IntegerProperty scoreProperty() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score.set(score);
+    }
+
+    public int getLife() {
+        return life.get();
+    }
+
+    public IntegerProperty lifeProperty() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life.set(life);
+    }
+
+
+    /**
+     * Ajouter 1 au score
+     */
+    public void addScore() {
+        score.set(score.get() + 1);
+    }
+
+    /**
+     * Retirer 1 point de vie
+     */
+    public void removeLife() {
+        life.set(life.get() - 1);
+    }
+
+    @Override
+    public void collidedWith(IMovable other) {
+        removeLife();
+    }
+
+    @Override
+    public void explode() {
+        addScore();
+    }
+
+    @Override
+    public void hitEnemy() {
+        /**
+         * Ne rien faire
+         */
+    }
+}
