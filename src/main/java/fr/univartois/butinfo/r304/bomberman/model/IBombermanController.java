f6e3b007ae7ee573df18d1000fc4d08af610493c
Bourhim Bylel <bylel_bourhim@ens.univ-artois.fr>
2024-10-14 08:55:26 +0200

    Correction de bug sur Player et BombermanGame

--- src/main/java/fr/univartois/butinfo/r304/bomberman/model/IBombermanController.java
+++ src/main/java/fr/univartois/butinfo/r304/bomberman/model/IBombermanController.java
@@ -16,7 +16,10 @@
 
 package fr.univartois.butinfo.r304.bomberman.model;
 
+import java.util.List;
+
 import fr.univartois.butinfo.r304.bomberman.model.map.GameMap;
+import fr.univartois.butinfo.r304.bomberman.model.movables.Bomb;
 import javafx.beans.binding.IntegerExpression;
 
 /**
@@ -53,9 +56,9 @@ public interface IBombermanController {
     /**
      * Lie le nombre de bombes du joueur à son affichage dans la vue.
      *
-     * @param bombsProperty La propriété stockant le nombre de bombes.
+     * @param list La propriété stockant le nombre de bombes.
      */
-    void bindBombs(IntegerExpression bombsProperty);
+    void bindBombs(List<Bomb> list);
 
     /**
      * Lie la vie du joueur à son affichage dans la vue.
